d8873bf1ba3e9f3ac73146cacae8748e58c32fd6
bbek4210 <bhandaribibek731@gmail.com>
2025-04-26 22:24:17 +0545

    fix

--- src/components/splash-screen.tsx
+++ src/components/splash-screen.tsx
@@ -6,8 +6,7 @@ const SplashScreen: React.FC = () => {
   return (
     <div className="flex items-center justify-center h-screen w-screen bg-[#5D4FFF]">
       <div className="animate-fade-in">
-        {/* <img src="zefewhitelogo.svg" alt="logo" /> */}
-        <h1>Zefe</h1>
+        <img src="zefewhitelogo.svg" alt="logo" />
       </div>
     </div>
   );
