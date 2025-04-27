a2037c5055c22ee46a19a9d1e871eba5a839aab5
Suzan Rana <dev.suzanrana@gmail.com>
2025-04-28 03:03:49 +0545

    fix: telegram mini appg

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,30 @@ import NetworksAndConnections from "@/pages/NetworksAndConnections";
 import UpdateProfile from "@/pages/UpdateUserProfile";
 import UserProfile from "@/pages/UserProfile";
 import HomePage from "@/pages/HomePage";
+import { useEffect } from "react";
 
-const App = () => (
-  <div className="p-4 ">
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/connected-user/:id" element={<ConnectedUser />} />
-      <Route
-        path="/networks-and-connections"
-        element={<NetworksAndConnections />}
-      />
-      <Route path="/update-user-profile" element={<UpdateProfile />} />
-      <Route path="/user" element={<UserProfile />} />
-    </Routes>
-  </div>
-);
+const App = () => {
+  useEffect(() => {
+    if (window.Telegram?.WebApp) {
+      window.Telegram.WebApp.ready();
+      window.Telegram.WebApp.expand();
+    }
+  }, []);
+
+  return (
+    <div className="p-4 ">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/connected-user/:id" element={<ConnectedUser />} />
+        <Route
+          path="/networks-and-connections"
+          element={<NetworksAndConnections />}
+        />
+        <Route path="/update-user-profile" element={<UpdateProfile />} />
+        <Route path="/user" element={<UserProfile />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
