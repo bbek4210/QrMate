9afabac2732ee726d4d5ca1908f55be494bb370b
Suzan Rana <dev.suzanrana@gmail.com>
2025-04-28 01:37:23 +0545

    fix: user profile

--- src/hooks/useUpdateUserProfile.ts
+++ src/hooks/useUpdateUserProfile.ts
@@ -4,13 +4,7 @@ import { useMutation } from "@tanstack/react-query";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const updateUserProfile = async (userData: any) => {
-  const token = getCookie("access_token");
-
-  const { data } = await axiosInstance.put("/userprofile/", userData, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+  const { data } = await axiosInstance.put("/userprofile/", userData);
 
   return data;
 };
