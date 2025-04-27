4b1c6c3def301ce221791e156717f2c072dd8b23
bbek4210 <bhandaribibek731@gmail.com>
2025-04-28 01:11:09 +0545

    fis

--- src/components/splash-screen.tsx
+++ src/components/splash-screen.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const SplashScreen: React.FC = () => {
   return (
-    <div className="flex items-center justify-center h-screen w-screen bg-[#5D4FFF]">
+    <div className="flex items-center justify-center h-screen w-screen bg-[#222222]">
       <div className="animate-fade-in">
         <img src="zefewhitelogo.svg" alt="logo" />
       </div>
